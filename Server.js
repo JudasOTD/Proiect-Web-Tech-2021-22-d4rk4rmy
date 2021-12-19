95bd07f7d0820c557725200d633897541e8a736c
Vlad <52937367+JudasOTD@users.noreply.github.com>
2021-12-19 17:24:24 +0200

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,4 @@
+// Acest fisier contine API-ul REST generat in node.js
+// Sunt implementate operatii de rutare cu obiect.json tip Review
+
+// Ulterior vom defini clasa Review si vom perfectiona viteza de redare a informatiilor
