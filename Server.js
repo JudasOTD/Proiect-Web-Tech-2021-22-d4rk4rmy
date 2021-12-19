0502145aa966a536a790efdf78217a72897d4d72
Vlad <52937367+JudasOTD@users.noreply.github.com>
2021-12-19 20:07:41 +0200

    Update Server.js
    basic CRUD routes
    documented code
    created transaction objects

--- Server.js
+++ Server.js
@@ -2,3 +2,68 @@
 // Sunt implementate operatii de rutare cu obiect.json tip Review
 
 // Ulterior vom defini clasa Review si vom perfectiona viteza de redare a informatiilor
+// Importarea modulelor
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+// Parseaza date ca json prin middleware
+app.use(express.json()) 
+app.use(bodyParser.json())
+
+app.use(express.urlencoded({extended:true})) // Parsare requesturi sub encoding UTF8 
+
+// Server config
+const PORT = 5500;
+app.listen(PORT, () => {
+    console.log("API is running... \nListening on port " + PORT)
+})
+
+// Default r00t 
+app.get('/pro', (req, res) => {
+    res.status(201).send(`The D4RK4RMY api. \nLive on port ${PORT}`)
+})
+
+// Redirect '/' to root
+app.get('/', (req, res) => {
+    
+})
+
+
+// Vizualizati toate notele printr-un GET general
+app.get('/comments', (req,res) => {
+    res.status(200).json(arrayReviews)
+})
+
+//get/projects  returneaza toate proiectele
+//get/projects:id  returneaza comentariile acordate unui proiect identificabil
+
+//post/projects adauga un proiect (validat prin input in SPA.html)
+//post/projects/:id .  adauga nota si comentariu la proiect
+
+//patch/projects/:idJurat  editeaza comentariul si nota in cazul in care un jurat vrea asta (trebuie cunoscut id-ul)
+//delete/project/:idJurat  sterge nota si comment
+
+// Vizualizati notele acordate de jurati unui anumit proiect
+//identificabil dupa idProiect. ID-ul juratilor este privat
+app.get('/pro/project/:id', (req,res) => {
+    res.status(200).json(arrayReviews)
+})
+
+
+// Obiect json cu notele livrabilelor
+// Acesta va fi preluat dintr-un fisier pentru eficienta in gestionarea intrarilor in urma acordarii de note
+const arrayReviews = [
+    {idProiect: 1, idJuriu: 10001, nrStelute: 10, comentariu: "Sursa excelenta de date."},
+    {idProiect: 2, idJuriu: 10002, nrStelute: 9, comentariu: "documentare pertinenta a instalatiilor"},
+    {idProiect: 3, idJuriu: 10020, nrStelute: 4, comentariu: "Angajament respectat in limitele extensiilor."},
+    {idProiect: 1, idJuriu: 10025, nrStelute: 10, comentariu: "Puteti migra?"},
+    {idProiect: 3,idJuriu: 10032, nrStelute: 1, comentariu: "spamspamspamspamspamspamspam"},
+    {idProiect: 2,idJuriu: 10040, nrStelute: 9, comentariu: "treaba bine facuta!!! clientii sunt multumiti"}
+]
+
+const arrayProiecte = [
+    {idProiect:1, denumire: "Sistem mailing"},
+    {idProiect: 2, denumire: "Site cinematografic"},
+    {idProiect: 3, denumire: "Test"}
+]
