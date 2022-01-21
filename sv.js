4b7752e1fd4f022d5294301140c3680700a394d0
Razvan87932 <91737824+Razvan87932@users.noreply.github.com>
2022-01-21 14:49:08 +0200

    Added server

--- sv.js
+++ sv.js
@@ -1,14 +1,19 @@
 // Importarea modulelor
 const bodyParser = require('body-parser')
 const express = require('express')
-const res = require('express/lib/response')
+const jsonfile = require("jsonfile");
+const cors = require("cors");
 const app = express()
 
-// Parsare JSON
+// Parseaza date ca json prin middleware
 app.use(express.json()) 
 app.use(bodyParser.json())
 
-app.use(express.json({extended:true})) // UTF8 
+app.use(express.json({extended:true})) // Parsare requesturi sub encoding UTF8 
+
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
 
 // Server config
 const PORT = 5500;
@@ -27,104 +32,172 @@ app.get('/', (req, res) => {
     res.redirect('/' + q)
 })
 
+// GET - returneaza lista de proiecte
+app.get('/projects', (req, res) => {
+    const file = jsonfile.readFileSync("./data.json");
 
-// Returneaza toate proiectele inscrise
-app.get('/projects', (req,res)=> {
-    res.status(200).json(arrayProiecte)
+    res.status(200).send(file.projects);
 })
 
-// Vizualizati notele si comentariile acordate de jurati unui anumit proiect
-//identificabil dupa idProiect. ID-ul juratilor este privat
-app.get('/project/:id', (req,res) => {
-    const id = req.params.id
-    try{
-      res.status(201).json(raport(id))  
-    } catch {
-        res.status(404).send(`Proiectul cu ID#${id} nu exista. Incercati altceva.`)
-    }
+// POST - creeaza un proiect
+app.post('/project', (req, res) => {
+    const file = jsonfile.readFileSync("./data.json");
+
+    // se aleg utilizatorii studenti care pot vota
+    const random = file.users.filter(x => x.role == "STUDENT").sort(() => .5 - Math.random()).slice(0,3);
+
+    // ia id-ul ultimului proiect si il incrementeaza cu 1
+    const id = file.projects[file.projects.length - 1].id + 1
+
+    // adaug in vectorul de proiecte noul proiect
+    file.projects.push({
+        ...req.body,
+        id,
+        // vectorul de note, initializat cu note de 0, deoarece nu au fost setate
+        notes: random.map(x => {
+            return {
+                user: x.email,
+                note: 0
+            }
+        }),
+        // livrabile gol, pentru ca nu au fost adaugate livrabile
+        livrabile: []
+    });
+
+    jsonfile.writeFileSync("./data.json", file);
+
+    res.status(201).send();
 })
 
-// Un profesor poate vizualiza toate notele acordate tuturor proiectelor
-app.get('/projects/prof', (req,res) => {
-    var jsonObj = []
-    for(let i = 0; i < arrayProiecte.length; i++) {
-        const a = raport(arrayProiecte[i].idProiect)
-        jsonObj.push(a)
+// GET - proiect dupa id
+app.get('/project/:id', (req, res) => {
+    const file = jsonfile.readFileSync("./data.json");
+
+    // cauta proiectul
+    const project = file.projects.find(x => x.id == req.params.id);
+
+    if (!project) {
+        return res.status(404).send();
     }
-    res.status(200).json(jsonObj)
+
+    res.status(200).send(project);
 })
 
-// POST/projects adauga un proiect (validat)
-app.post('/project', (req, res)=>{
-    console.log('Ruta POST pentru adaugarea de proiecte a fost apelata.')
-    try{
-       if(req.body.denumire.length === 0 || req.body.denumire === null){
-        res.status(406).send("\nNu pot fi create obiecte fără nume.")
-    } else  {
-        const denumire = req.body.denumire
-        const pj = {
-        "idProiect" : ++contorGlobal,
-        denumire }
-        arrayProiecte.push(pj) 
-        res.status(201).send("\nProiect inregistrat cu succes.\nIdentificabil dupa ID#" + contorGlobal)
-    } 
-    } catch(e){
-        console.log(e.name)
-        res.status(400).send(e.name)
+// GET - livrabilele unui proiect dupa id
+app.get('/project/livrabile/:id', (req, res) => {
+    const file = jsonfile.readFileSync("./data.json");
+
+    const project = file.projects.find(x => x.id == req.params.id);
+
+    if (!project) {
+        return res.status(404).send();
     }
-    
-    
+
+    res.status(200).send(project.livrabile);
 })
 
-//post/project/:id .  adauga nota si comentariu la proiect
+// GET - livrabil dupa id al unui proiect
+app.get('/livrabil/:proiectid/:id', (req, res) => {
+    const file = jsonfile.readFileSync("./data.json");
 
-//patch/project/:idJurat editeaza comentariul si nota in cazul in care un jurat vrea asta (trebuie cunoscute credentialele)
-//delete/project/:idJurat sterge comment
+    const project = file.projects.find(x => x.id == req.params.proiectid);
 
-
-
-
-// Obiecte json cu notele livrabilelor
-// Ele pot fi preluate dintr-un fisier
-//pentru eficienta in gestionarea intrarilor in urma acordarii de note
-const arrayReviews = [
-    {idProiect: 1, idJuriu: 10001, nrStelute: 10, comentariu: "Sursa excelenta de date."},
-    {idProiect: 2, idJuriu: 10002, nrStelute: 9, comentariu: "documentare pertinenta a instalatiilor"},
-    {idProiect: 3, idJuriu: 10020, nrStelute: 4, comentariu: "Angajament respectat in limitele extensiilor."},
-    {idProiect: 1, idJuriu: 10025, nrStelute: 10, comentariu: "Puteti migra?"},
-    {idProiect: 3,idJuriu: 10032, nrStelute: 1, comentariu: "spamspamspamspamspamspamspam"},
-    {idProiect: 2,idJuriu: 10040, nrStelute: 9, comentariu: "treaba bine facuta!!! clientii sunt multumiti"}
-]
-
-const arrayProiecte = [
-    {idProiect:1, denumire: "Sistem mailing"},
-    {idProiect: 2, denumire: "Site cinematografic"},
-    {idProiect: 3, denumire: "Test"}
-]
-
-
-var contorGlobal = arrayProiecte.length
-
-const raport = function(ID) {
-    //ID este preluat din req.params si transmis sub forma de  string
-    //ID trebuie convertit
-    var proj = arrayProiecte.find((p) => p.idProiect === parseInt(ID))
-    var comm = arrayReviews.filter(function(obj) {
-        return obj.idProiect === parseInt(ID)
-    })
- 
-    var note = new Array()
-    var comentarii = new Array()
-    for(let i = 0; i < comm.length; i++) {
-        note.push(comm[i].nrStelute)
-        comentarii.push(comm[i].comentariu)
+    if (!project) {
+        return res.status(404).send();
     }
 
-    const raport = {
-        "idProiect" : ID,
-        "denumire" : proj.denumire,
-        "note" : note,
-        "comentarii" : comentarii
+    const livrabil = project.livrabile.find(x => x.id == req.params.id)
+
+    if (!livrabil) {
+        return res.status(404).send();
     }
-    return(raport)
-}
+
+    res.status(200).send(livrabil);
+});
+
+// POST - creeaza livrabil la proiect
+app.post("/livrabil/:proiectid", (req, res) => {
+    const file = jsonfile.readFileSync("./data.json");
+
+    // cautam pozitia proiectului in vector
+    const projectId = file.projects.findIndex(x => x.id == req.params.proiectid);
+
+    if (projectId == -1) {
+        return res.status(404).send();
+    }
+
+
+    // setarea id-ului
+    let id;
+
+    // daca nu exista livrabile pe proiect, atunci id va lua valoarea 1
+    if (file.projects[projectId].livrabile.length == 0) {
+        id = 1
+    } else {
+        // in caz contrar o sa iau ultimul livrabil si o sa incrementez id-ul cu 1
+        id = file.projects[projectId].livrabile[file.projects[projectId].livrabile.length - 1].id + 1;
+    }
+
+    // adaug livrabilul la vectorul de livrabile
+    file.projects[projectId].livrabile.push({
+        // name, description, video
+        ...req.body,
+        id: id,
+        date: new Date(),
+        completed: false
+    });
+
+    jsonfile.writeFileSync("./data.json", file);
+
+    res.status(200).send();
+})
+
+// POST - noteaza proiect
+app.post("/note/:id", (req, res) => {
+    const file = jsonfile.readFileSync("./data.json");
+
+    const projectId = file.projects.findIndex(x => x.id == req.params.id);
+
+    if (projectId == -1) {
+        return res.status(404).send();
+    }
+
+
+    // mail-ul este trimis ca si header din frontend, si este folosit pentru a autoriza utilizatorii
+    const email = req.get("X-Email")
+
+    // cauta nota utilizatorului
+    const notaId = file.projects[projectId].notes.findIndex(x => x.user == email)
+
+    // daca utilizatorul nu poate vota
+    if (notaId == -1) {
+        return res.status(404).send();
+    }
+
+    // pune nota
+    file.projects[projectId].notes[notaId].note = req.body.note;
+
+    jsonfile.writeFileSync("./data.json", file);
+
+    res.status(200).send();
+});
+
+// GET - login
+app.get("/status", (req, res) => {
+    const file = jsonfile.readFileSync("./data.json");
+
+    const email = req.get("X-Email")
+    const password = req.get("X-Password")
+
+    console.log(email, password)
+
+    // se cauta utilizatorul dupa mail si parola
+    const user = file.users.find(x => x.email == email && x.password == password)
+
+    if (!user) {
+        // 403 - neautorizat
+        return res.status(403).send();
+    }
+
+    res.status(200).send(user)
+})
